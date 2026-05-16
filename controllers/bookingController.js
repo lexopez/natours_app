0836b29fc3778af2b691e08e6ac252b49c8ca234
alexander lopez <iamlexlopez@gmail.com>
2026-05-16 08:04:45 +0800

    update prop for accessing session total amount

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -38,6 +38,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     ],
   });
 
+  // console.log(session);
+
   // 3) Create session as response
   res.status(200).json({
     status: 'success',
@@ -48,7 +50,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  // Modern Stripe uses amount_total (in cents)
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
