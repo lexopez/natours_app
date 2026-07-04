97f5413811b9ae4fe408f6b880ef62e3bb538b98
alexander lopez <iamlexlopez@gmail.com>
2026-07-04 11:34:59 +0800

    update email

--- utils/email.js
+++ utils/email.js
@@ -1,71 +1,3 @@
-// const nodemailer = require('nodemailer');
-// const pug = require('pug');
-// const htmlToText = require('html-to-text');
-
-// module.exports = class Email {
-//   constructor(user, url) {
-//     this.to = user.email;
-//     this.firstName = user.name.split(' ')[0];
-//     this.url = url;
-//     this.from = `alexander lopez <${process.env.EMAIL_FROM}>`;
-//   }
-
-//   newTransport() {
-//     if (process.env.NODE_ENV === 'production') {
-//       // Sendgrid
-//       return nodemailer.createTransport({
-//         service: 'SendGrid',
-//         auth: {
-//           user: process.env.SENDGRID_USERNAME,
-//           pass: process.env.SENDGRID_PASSWORD,
-//         },
-//       });
-//     }
-
-//     return nodemailer.createTransport({
-//       host: process.env.EMAIL_HOST,
-//       port: process.env.EMAIL_PORT,
-//       auth: {
-//         user: process.env.EMAIL_USERNAME,
-//         pass: process.env.EMAIL_PASSWORD,
-//       },
-//     });
-//   }
-
-//   // Send the actual email
-//   async send(template, subject) {
-//     // 1) Render HTML based on a pug template
-//     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
-//       firstName: this.firstName,
-//       url: this.url,
-//       subject,
-//     });
-
-//     // 2) Define email options
-//     const mailOptions = {
-//       from: this.from,
-//       to: this.to,
-//       subject,
-//       html,
-//       text: htmlToText.fromString(html),
-//     };
-
-//     // 3) Create a transport and send email
-//     await this.newTransport().sendMail(mailOptions);
-//   }
-
-//   async sendWelcome() {
-//     await this.send('welcome', 'Welcome to the Natours Family!');
-//   }
-
-//   async sendPasswordReset() {
-//     await this.send(
-//       'passwordReset',
-//       'Your password reset token (valid for only 10 minutes)',
-//     );
-//   }
-// };
-
 const nodemailer = require('nodemailer');
 const pug = require('pug');
 const htmlToText = require('html-to-text');
@@ -79,37 +11,27 @@ module.exports = class Email {
   }
 
   newTransport() {
-    // if (process.env.NODE_ENV === 'production') {
-    //   console.log("CurrentEnvironment: ", process.env.NODE_ENV);
-    //   // Production: Brevo (Sendinblue) SMTP settings
-    //   return nodemailer.createTransport({
-    //     host: process.env.BREVO_HOST || 'smtp-relay.brevo.com',
-    //     port: process.env.BREVO_PORT || 587,
-    //     auth: {
-    //       user: process.env.BREVO_USERNAME, // Your Brevo login email
-    //       pass: process.env.BREVO_PASSWORD, // Your Brevo Master SMTP Key
-    //     },
-    //   });
-    // }
+    if (process.env.NODE_ENV === 'production') {
+      // Production: Brevo (Sendinblue) SMTP settings
+      return nodemailer.createTransport({
+        host: process.env.BREVO_HOST || 'smtp-relay.brevo.com',
+        port: process.env.BREVO_PORT || 587,
+        auth: {
+          user: process.env.BREVO_USERNAME, // Your Brevo login email
+          pass: process.env.BREVO_PASSWORD, // Your Brevo Master SMTP Key
+        },
+      });
+    }
 
+    // Development: Mailtrap
     return nodemailer.createTransport({
-      host: process.env.BREVO_HOST || 'smtp-relay.brevo.com',
-      port: process.env.BREVO_PORT || 587,
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
       auth: {
-        user: process.env.BREVO_USERNAME, // Your Brevo login email
-        pass: process.env.BREVO_PASSWORD, // Your Brevo Master SMTP Key
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
-
-    // // Development: Mailtrap
-    // return nodemailer.createTransport({
-    //   host: process.env.EMAIL_HOST,
-    //   port: process.env.EMAIL_PORT,
-    //   auth: {
-    //     user: process.env.EMAIL_USERNAME,
-    //     pass: process.env.EMAIL_PASSWORD,
-    //   },
-    // });
   }
 
   // Send the actual email
